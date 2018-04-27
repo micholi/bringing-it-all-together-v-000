9a51353f095c15f01de8bb482904b92205c52696
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 15:55:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,28 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
 
+  end
+
+  def.self.drop_table
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
+  def save
+
+  end
 
 end
