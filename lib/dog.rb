491ec92a4c23d4c8e84b98d6c04e5651320b5994
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 16:18:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,11 @@ class Dog
 
   def self.find_or_create_by
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?, name, breed")
+
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    end
   end
 
   def update
